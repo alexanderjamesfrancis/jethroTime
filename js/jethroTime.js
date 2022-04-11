cded4f855213d70dfd6b783456160a205de27fd0
alexanderjamesfrancis <100845219+alexanderjamesfrancis@users.noreply.github.com>
2022-04-11 15:02:50 +0100

    Add files via upload

---
+++ js/jethroTime.js
@@ -0,0 +1,21 @@
+function current_time(){
+  var d = new Date();
+  var time = d.toLocaleTimeString();
+  return time;
+
+};
+
+
+var now_time = document.getElementsByClassName("gmt_time")[0].innerHTML = current_time();
+
+function its_Jethro_Time(){
+  var hour = docu
+}
+
+function randomNumber() {
+  number  = Math.floor(Math.random() * 59)
+  return number
+};
+
+var timeControl = document.querySelector('input[type="time"]');
+timeControl.value = '15:30';
